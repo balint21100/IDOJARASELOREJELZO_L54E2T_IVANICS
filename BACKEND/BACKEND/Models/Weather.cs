2a970848426489455ae57e53fa04d73f54a384e3
balint21100 <ivanics.balint21@gmail.com>
2025-05-11 23:57:22 +0200

    Weather osztály létrehozása

---
+++ BACKEND/BACKEND/Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace BACKEND.Models
+{
+    public class Weather
+    {
+        public string Forecast { get; set; }
+        public int C { get; set; }
+        public int Windspeed { get; set; }
+    }
+}
