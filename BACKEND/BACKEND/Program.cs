049654edf02c7ba1b37c8da77e884be38df13c8e
balint21100 <ivanics.balint21@gmail.com>
2025-05-11 23:54:44 +0200

    Swagger implementálása

--- BACKEND/BACKEND/Program.cs
+++ BACKEND/BACKEND/Program.cs
@@ -3,11 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors(x => x
     .AllowAnyOrigin()
